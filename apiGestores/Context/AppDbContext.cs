c24342081f1db73ab9045509e1c1fb23021b3df3
Hilmer <hilserrano1520@gmail.com>
2021-12-18 15:40:30 -0600

    Add project files.

---
+++ apiGestores/Context/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using apiGestores.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace apiGestores.Context
+{
+    public class AppDbContext: Microsoft.EntityFrameworkCore.DbContext
+    {
+        public  AppDbContext(DbContextOptions <AppDbContext> options): base(options)
+        {
+
+        }
+
+        public Microsoft.EntityFrameworkCore.DbSet<Gestores_Bd> gestores_bd { get; set;}
+       
+
+     
+    }
+}
